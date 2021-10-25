3efa6203e632331cd9fa2dc898d7e77f9114848e
Kade Jensen <kadejensen2@gmail.com>
2021-10-25 15:48:57 -0500

    dice say value not read

--- yahtzee/src/app/components/scoreblock/scoreblock.component.ts
+++ yahtzee/src/app/components/scoreblock/scoreblock.component.ts
@@ -14,10 +14,25 @@ export class ScoreblockComponent implements OnInit {
   value:any = null
   signal!: string;
   category! : string;
+  dice=["1","2","3","4","5"]
   constructor(private _rollService: rollService){
     this._rollService.listen().subscribe((m:any) => {
-        console.log(m, "in score");
-        if (m ==1){this.diceValues[0]= m}
+     if(m in this.dice){
+      for (var _i = 0; _i <this.diceValues.length ; _i++) {
+      this.diceValues[_i] = m}
+     }
+      //console.log(_i)}
+      //console.log(m);
+        if (m== 1){this.diceValues[0]= m}
+        // if (m== 2){this.diceValues[1]= m}
+        // if (m== 3){this.diceValues[2]= m}
+        // if (m== 4){this.diceValues[3]= m}
+        // if (m== 5){this.diceValues[4]= m}
+        // if (m== 6){this.diceValues[5]= m}
+        if(m=="print"){
+          this.printNum()
+        }
+
 
     })
   }
@@ -31,7 +46,9 @@ export class ScoreblockComponent implements OnInit {
     console.log(this.signal)
     this.addPoints()
   }
-
+  printNum(){
+    console.log(this.diceValues)
+  }
 
   addPoints(){
     if (this.category == "Full House"){
